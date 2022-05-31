4ef0fafefb41f8fc0ad3ccce5fc9c6a09eca7d57
Salhi Ali <salhiali197@gmail.com>
2022-05-31 23:19:55 +0100

    email setup and confirm 2.2

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -17,10 +17,11 @@ class UserController extends Controller
     public function verifyEmail($code_email)
     {
         $user = User::where('code_email',$code_email)->first();
+        dd($user);
         if(is_null($user)){
             return redirect()->route('home')->with('success', 'Success ');        
         }else{
-            dd('saz');
+            dd('saz');  
             Auth::logout();
             $user->email_verified = 1;
             $user->save();
