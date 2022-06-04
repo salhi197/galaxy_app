d8aa793fa3d92b79f18dd42f57b5f6e3ca411d92
Salhi Ali <salhiali197@gmail.com>
2022-06-04 14:32:21 +0100

    add certificate 2.2

--- app/Template.php
+++ app/Template.php
@@ -39,6 +39,11 @@ class Template extends Authenticatable
         <div style="position:absolute;top:4.40in;left:0.83in;width:4.38in;line-height:0.27in;"><DIV style="position:relative; left:1.55in;"><span style="font-style:normal;font-weight:bold;font-size:15pt;font-family:Swis721 Ex BT;color:#e1cc8e">DURÉ </span></SPAN></DIV><br/></div>
         <div style="position:absolute;top:4.40in;left:0.83in;width:4.38in;line-height:0.27in;"><DIV style="position:relative; left:2.41in;"><span style="font-style:normal;font-weight:bold;font-size:15pt;font-family:Swis721 Ex BT;color:#e1cc8e">D’UNE </span></SPAN></DIV><br/></div>
         <div style="position:absolute;top:4.40in;left:0.83in;width:4.38in;line-height:0.27in;"><DIV style="position:relative; left:3.36in;"><span style="font-style:normal;font-weight:bold;font-size:15pt;font-family:Swis721 Ex BT;color:#e1cc8e">ANNÉE</span><span style="font-style:normal;font-weight:bold;font-size:15pt;font-family:Swis721 Ex BT;color:#e1cc8e"> </span><br/></SPAN></DIV></div>
+
+
+        <div style="position:absolute;top:5.40in;left:0.83in;width:4.38in;line-height:0.27in;"><DIV style="position:relative; left:0.88in;"><span style="font-style:normal;font-weight:bold;font-size:15pt;font-family:Swis721 Ex BT;color:#e1cc8e">'.$user->name.' </span></SPAN></DIV><br/></div>
+        <div style="position:absolute;top:5.40in;left:0.83in;width:4.38in;line-height:0.27in;"><DIV style="position:relative; left:1.55in;"><span style="font-style:normal;font-weight:bold;font-size:15pt;font-family:Swis721 Ex BT;color:#e1cc8e">'.$user->nom.' </span></SPAN></DIV><br/></div>
+
         <img style="position:absolute;top:1.79in;left:0.56in;width:3.21in;height:1.34in" src="'.asset('img/pdf/ri_1.png').'" />
         <img style="position:absolute;top:4.26in;left:7.23in;width:0.45in;height:0.71in" src="'.asset('img/pdf/ri_2.png').'" />
         <div style="position:absolute;top:0.31in;left:3.14in;width:2.93in;line-height:0.48in;"><span style="font-style:normal;font-weight:bold;font-size:26pt;font-family:Swis721 Ex BT;color:#c7ac63">CERTIFICAT</span><span style="font-style:normal;font-weight:bold;font-size:26pt;font-family:Swis721 Ex BT;color:#c7ac63"> </span><br/></SPAN></div>
