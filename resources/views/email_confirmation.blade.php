3c33684a86dd60a6059e7b2d2594c34669af74cc
Salhi Ali <salhiali197@gmail.com>
2022-07-11 09:38:01 +0100

    fix email 2.0

--- resources/views/email_confirmation.blade.php
+++ resources/views/email_confirmation.blade.php
@@ -1,4 +1,4 @@
-Clqiuer Sur le Lien ci-dessus pour confirmer Votre Compte  : 
+Veuillez Cliquer Sur le Lien ci-dessus pour confirmer Votre Compte  : 
 
 <a href="https://app.mygalaxy.world/user/email/{{$dataEmail['code']}}">
     confirmer votre email
