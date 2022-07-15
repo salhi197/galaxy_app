7a2646fabf0404079c02df00b8921030adcbcaa7
Salhi Ali <salhiali197@gmail.com>
2022-07-15 11:53:34 +0100

    fix email message

--- resources/views/bienvenue.blade.php
+++ resources/views/bienvenue.blade.php
@@ -1,5 +1,5 @@
-Congratulations ! 
-Dear {{$data['name']}} et {{$data['nom']}}<br>
+Congratulations !<br> 
+Dear {{$data['name']}} {{$data['nom']}}<br>
 
 Merci d'avoir rejoint notre communauté et de vous être inscrit sur notre premium plateforme . <br>
 Nous avons pour mission de fournir aux utilisateurs le savoir faire et la bonne formation et les bonnes ressources pour rendre 
